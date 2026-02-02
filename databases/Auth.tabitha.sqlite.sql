5986a0ff7b51bb181c79fd35dac3d1e72a9838a2
craigp-atw <152427351+craigp-atw@users.noreply.github.com>
2026-02-02 10:42:51 -0800

    Add ontology permissions (#29)

--- databases/Auth.tabitha.sqlite.sql
+++ databases/Auth.tabitha.sqlite.sql
@@ -1,4 +1,17 @@
+CREATE TABLE Permissions (
+			id				INTEGER PRIMARY KEY,
+			app			TEXT,
+			permission	TEXT
+		);
+INSERT INTO Permissions VALUES(1,'ontology','PROTECTED_ACCESS');
+INSERT INTO Permissions VALUES(2,'ontology','ADD_CONCEPT');
+INSERT INTO Permissions VALUES(3,'ontology','UPDATE_CONCEPT');
+INSERT INTO Permissions VALUES(4,'ontology','DELETE_CONCEPT');
+CREATE TABLE User_Permissions (
+			user_email	TEXT,
+			permission_id	INTEGER
+		);
 CREATE TABLE Users (
-		email TEXT PRIMARY KEY,
-		app TEXT
-	);
+			email		TEXT PRIMARY KEY,
+			name		TEXT
+		);
