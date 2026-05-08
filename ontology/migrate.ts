092c4c121ae72dca4093b6a028ca1ddde96ef22d
billy clark <billy.clark@presciencelabs.org>
2026-05-08 14:20:53 -0400

    needed a little more logging

--- ontology/migrate.ts
+++ ontology/migrate.ts
@@ -3,6 +3,9 @@ import { load_examples } from './exhaustive_examples/load'
 import { migrate_complex_terms_table } from './migrate_complex_terms_table'
 
 // usage: `bun ontology/migrate.ts databases/Sources_YYYY-MM-DD.tabitha.sqlite databases/Ontology_VERSION_YYYY-MM-DD.tabitha.sqlite`
+if (Bun.argv.length !== 4) {
+	throw new Error('Usage: bun ontology/migrate.ts databases/Sources_YYYY-MM-DD.tabitha.sqlite databases/Ontology_VERSION_YYYY-MM-DD.tabitha.sqlite')
+}
 const tabitha_db_name = Bun.argv[3]	// databases/Ontology_VERSION_YYYY-MM-DD.tabitha.sqlite
 
 const tabitha_db = new Database(tabitha_db_name, { create: false, readwrite: true })
@@ -13,10 +16,16 @@ tabitha_db.run('PRAGMA journal_mode = WAL')
 await migrate_complex_terms_table(tabitha_db)
 
 const sources_db_name = Bun.argv[2]	// databases/Sources_YYYY-MM-DD.tabitha.sqlite
+console.log(`[Ontology migration] Opening Sources database: ${sources_db_name}`)
 const sources_db = new Database(sources_db_name)
-const sources_db_complex = new Database(sources_db_name.replace('Sources', 'Sources_Complex'), { readwrite: true, create: false })
+
+const sources_db_complex_name = sources_db_name.replace('Sources', 'Sources_Complex')
+console.log(`[Ontology migration] Opening Sources_Complex database: ${sources_db_complex_name}`)
+const sources_db_complex = new Database(sources_db_complex_name, { readwrite: true, create: false })
+
 await load_examples(tabitha_db, sources_db, sources_db_complex)
 
 console.log(`Optimizing ${tabitha_db_name}...`)
 tabitha_db.run(`VACUUM`)
 console.log('done.')
+tabitha_db.close()
