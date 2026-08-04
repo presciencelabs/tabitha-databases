24c0dec2135e55fae0307dba3887767714d821c3
craigp_atw <craig_palmer@alltheword.org>
2026-08-04 14:21:07 -0700

    Add ontology Changes table

---
+++ ontology/changes.ts
@@ -0,0 +1,19 @@
+import type { Database } from 'bun:sqlite'
+
+export function create_changes_table(tabitha_db: Database) {
+	console.log(`Creating Changes table in ${tabitha_db.filename}...`)
+
+	tabitha_db.run(`
+		CREATE TABLE IF NOT EXISTS Changes (
+			'concept_stem'					TEXT,
+			'concept_sense' 				TEXT,
+			'concept_part_of_speech'	TEXT,
+			'action'							TEXT,
+			'data'							TEXT,
+			'approved_by_email'			TEXT,
+			'approved_date,				TEXT
+		)
+	`)
+
+	console.log('done.')
+}
